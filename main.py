2dcc22569ebb0cb2a50a95c93ab5e8ec74c9fe3d
Hubert Fauconnier <h.fauconnier@gmail.com>
2025-06-27 11:43:15 +0200

    remove docstring

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python3
-"""
-Automatisation de la sauvegarde des pièces jointes Slack
-Exporte automatiquement l'historique Slack et télécharge les pièces jointes
-"""
-
 from orchestration import Orchestration
 
 
