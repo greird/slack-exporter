029d0c7cd32c241a594d66b747e41ce226e7276d
Hubert Fauconnier <h.fauconnier@gmail.com>
2025-07-07 16:42:44 +0200

    update main.py

--- slack_exporter/main.py
+++ slack_exporter/main.py
@@ -12,28 +12,46 @@ from slack_exporter.etl import (
 
 load_dotenv(dotenv_path=find_dotenv(raise_error_if_not_found=True))
 
+# GLOBAL CONFIG
+local_dir = f"./slack_backups_{datetime.now().strftime('%Y%m%d_%H%M%S')}"
+oldest_timestamp = (datetime.now() - timedelta(days=90)).timestamp() # last 90 days of data
+
+# MEGA CONFIG
+mega_credentials = {
+    "login": os.getenv("MEGA_EMAIL"), 
+    "password": os.getenv("MEGA_PASSWORD")
+}
+
+# GOOGLE DRIVE CONFIG
+google_drive_parent_dir = os.getenv("GOOGLE_DRIVE_PARENT_FOLDER_ID")
+google_drive_credentials_path = os.getenv("GOOGLE_DRIVE_CREDENTIALS_PATH")
+
 if __name__ == "__main__":
     logger.info("=== Starting Slack backup ===")
-    # Uncomment the line below to run the Mega.io export
-    SlackToMega(
-        local_dir=f"./slack_backups_{datetime.now().strftime('%Y%m%d_%H%M%S')}",
-        remote_dir="",
-        credentials={"login": os.getenv("MEGA_EMAIL"), "password": os.getenv("MEGA_PASSWORD")},
-        file_suffix=f"_{datetime.now().strftime('%Y%m%d_%H%M%S')}",
-        oldest_timestamp=(datetime.now() - timedelta(days=90)).timestamp()
-    ).run()
 
-    # SlackToLocalWithCompression(
-    #     local_dir=f"./slack_backups_{datetime.now().strftime('%Y%m%d_%H%M%S')}",
-    #     oldest_timestamp=(datetime.now() - timedelta(days=30)).timestamp()
+    # Uncomment the lines below to run the export of your choice
+
+    # # Export and store data locally
+    # SlackToLocal(
+    #     local_dir=local_dir,
+    #     oldest_timestamp=oldest_timestamp
     # ).run()
 
-    # Uncomment the line below to run the Google Drive export
+    # Export all in a slack_backup folder, add a timestamp as a suffix to each file and upload at the root of the Mega directory.
+    SlackToMega(
+        local_dir="./slack_backup",
+        remote_dir="",
+        credentials=mega_credentials,
+        file_suffix=f"_{datetime.now().strftime('%Y%m%d_%H%M%S')}",
+        oldest_timestamp=oldest_timestamp
+    ).run()
+
+    # # Export and upload local_dir to a remote_folder in Google Drive.
     # SlackToGoogleDrive(
-    #     local_dir=f"./slack_backups_{datetime.now().strftime('%Y%m%d_%H%M%S')}",
-    #     remote_dir='1Ylrd8evEMdc_LCEWER6wttdQqO8JZck2',
-    #     credentials='credentials.json',
-    #     oldest_timestamp=(datetime.now() - timedelta(days=90)).timestamp()
+    #     local_dir=local_dir,
+    #     remote_dir=google_drive_parent_dir,
+    #     credentials=google_drive_credentials_path,
+    #     oldest_timestamp=oldest_timestamp
     # ).run()
 
     logger.info("=== Slack backup completed ===")
